af1464c21a22a9c48a4f360d70eb3d3d6c459695
Nikko Baez <Nikkobaez82@gmail.com>
2023-08-24 16:01:59 -0500

    Portfolio Website
    Version 1.2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 // import React, { useState, useEffect } from 'react'
 import HeroImage from '../assets/heroimage.png'
 import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
+import { Link } from 'react-scroll'
 // import Typed from 'react-typed'
 const Home = () => {
     // const [showCursor, setShowCursor] = useState(true)
@@ -28,10 +29,14 @@ const Home = () => {
                     </p>
                     <div>
                         <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 duration-200'>
-                            Portfolio
-                            <span className='group-hover:rotate-90 duration-300'>
-                                <MdOutlineKeyboardArrowRight size={25} className='ml-1'/>
-                            </span>
+                            <Link to={"portfolio"} smooth duration={500}>           
+                                <div className="flex">
+                                    Portfolio
+                                    <span className='group-hover:rotate-90 duration-300'>
+                                        <MdOutlineKeyboardArrowRight size={25} className='ml-1'/>
+                                    </span>
+                                </div>
+                            </Link>
                         </button>
                     </div>
                 </div>
