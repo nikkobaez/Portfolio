ac00b42d25cec5c45e917b8a434a8a9dc303b3f8
Nikko Baez <Nikkobaez82@gmail.com>
2023-08-31 15:34:57 -0500

    Portfolio Website
    Version 1.4

--- src/components/About.js
+++ src/components/About.js
@@ -10,19 +10,19 @@ const About = () => {
                     </p>
                 </div>
                 <p className='text-md sm: text-l'>
-                    Having graduated from Texas A&M in 2020 with a degree in Biomedical Science and initial aim to pursue advanced studies, my journey into the tech world began when I discovered my passion for coding and started studying web development in 2022. Over the course of a year using reputable platforms like the Odin Project and FreeCodeCamp, I gained a solid understanding of several programming languages, frameworks, developer tools, and libraries such as HTML, CSS, JavaScript, React, TailwindCSS, and Firebase. These skills gave me the ability to construct visually appealing and dynamic user interfaces and applications. 
+                    After graduating from Texas A&M University in 2020 with a bachelor’s degree in Biomedical Science, my plan was to pursue further studies and apply to dental school. It was during this time though I discovered my passion for coding and started studying web development in 2022. Over the course of a year, using reputable platforms such as CodeAcademy, The Odin Project, FreeCodeCamp, and Udemy I gained a solid understanding of several programming languages, frameworks, developer tools, and libraries including HTML, CSS, JavaScript, React, TailwindCSS, and Firebase. These skills have given me the ability to develop visually appealing and dynamic web applicaitons. 
                 </p>
 
                 <br/>
 
                 <p className='text-md sm:text-l'>
-                    Wanting to focus on mastery of a specific technology and gain a solid understanding of computer science concepts, I decided to start studying iOS development in 2023 as well as enroll at the University of Houston to pursue a bachelor’s degree in Computer Science. Over the course of the last few months drawing knowledge from resources such as Kodeo (formerly Raywenderlich), Hacking with Swift, iOS programming texts by Mark Neuburg, and my college courses I have learned a variety of new programming languages and frameworks including C++, Swift, SwiftUI, and UIKit.  
+                    Wanting to focus on mastery of a specific technology due to the wide variety avaliable for web development and gain a solid understanding of computer science concepts, I decided to start studying iOS development in 2023 as well as enroll at the University of Houston to pursue a bachelor’s degree in Computer Science. Over the course of the past few months drawing knowledge from resources such as Kodeco, Hacking with Swift, and my college courses I have learned a variety of new programming languages and frameworks including C++, Swift, SwiftUI, and UIKit.
                 </p>
 
                 <br/>
 
                 <p className='text-l'>
-                    My transition from a scientific background to the world of programming showcases my adaptability and commitment to continuous learning and personal development. Blending my diverse insights with technical proficiency, I am dedicated to producing impacting and lasting results in the world of technology. Join me in this exciting venture, where I transform innovative ideas into tangible software solutions.  Be it crafting exceptional web interfaces or architecting immersive iOS experiences, I am resolute in my pursuit of excellence and drive to make a difference in helping startups to fourtune 500 companies.
+                    I am still in the process of learning iOS development, but in the meantime I am also in the works of preparing and setting up my startup BanesTech a company that provides new and small businesses access to web and mobile applicaitons at an affordable rate. In conclusion, my transition from a scientific background to the world of programming showcases my adaptability and commitment to continuous learning and personal development. Blending my diverse insights with technical proficiency, I am dedicated to producing impacting and lasting results in the world of technology.
                 </p>
             </div>
         </div>
